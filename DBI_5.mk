23d3769797e5f628d1432a2bdecf27224fd6e650
Andrey Nikiforov <ankifor@debian-ankifor.ankifor>
2016-11-29 01:12:58 +0100

    compilation file added

--- DBI_5.mk
+++ DBI_5.mk
@@ -2,12 +2,12 @@
 ## Auto Generated makefile by CodeLite IDE
 ## any manual changes will be erased      
 ##
-## Debug
+## Release
 ProjectName            :=DBI_5
-ConfigurationName      :=Debug
+ConfigurationName      :=Release
 WorkspacePath          :=/home/ankifor/Documents/CPP
 ProjectPath            :=/home/ankifor/Documents/CPP/DBI_5
-IntermediateDirectory  :=./Debug
+IntermediateDirectory  :=./Release
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
@@ -28,7 +28,7 @@ LibraryPathSwitch      :=-L
 PreprocessorSwitch     :=-D
 SourceSwitch           :=-c 
 OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
-Preprocessors          :=
+Preprocessors          :=$(PreprocessorSwitch)NDEBUG 
 ObjectSwitch           :=-o 
 ArchiveOutputSwitch    := 
 PreprocessOnlySwitch   :=-E
@@ -50,8 +50,8 @@ LibPath                := $(LibraryPathSwitch).
 AR       := /usr/bin/ar rcu
 CXX      := /usr/bin/g++
 CC       := /usr/bin/gcc
-CXXFLAGS :=  -g -O0 -std=c++11 -Wall $(Preprocessors)
-CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
+CXXFLAGS :=  -O3 -fexpensive-optimizations -std=c++11 -Wall $(Preprocessors)
+CFLAGS   :=  -O2 -Wall $(Preprocessors)
 ASFLAGS  := 
 AS       := /usr/bin/as
 
@@ -79,11 +79,11 @@ $(OutputFile): $(IntermediateDirectory)/.d $(Objects)
 	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
 
 MakeIntermediateDirs:
-	@test -d ./Debug || $(MakeDirCommand) ./Debug
+	@test -d ./Release || $(MakeDirCommand) ./Release
 
 
 $(IntermediateDirectory)/.d:
-	@test -d ./Debug || $(MakeDirCommand) ./Debug
+	@test -d ./Release || $(MakeDirCommand) ./Release
 
 PreBuild:
 
@@ -161,6 +161,6 @@ $(IntermediateDirectory)/Types.cpp$(PreprocessSuffix): Types.cpp
 ## Clean
 ##
 clean:
-	$(RM) -r ./Debug/
+	$(RM) -r ./Release/
 
 
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <dlfcn.h>
 #include <regex>
+#include <chrono>
 #include "Schema.hpp"
 #include "Parser_Schema.hpp"
 #include "Parser_Query.h"
@@ -60,10 +61,11 @@ static void read_data(const string& path)
 }
 
 int main(int argc, char* argv[]) {
-	if (argc != 4) {
+	if (argc != 5) {
 		cerr << "usage: " << argv[0] 
 		     << " <schema file>" 
 		     << " <source folder>"
+		     << " <object folder>"
 			 << " <data dir>"
 		     << endl
 		     << argc << endl;
@@ -72,7 +74,8 @@ int main(int argc, char* argv[]) {
 	
 	string schema_path = string(argv[1],strlen(argv[1]));
 	string source_path = string(argv[2],strlen(argv[2])) + "/";
-	string path_data = string(argv[3],strlen(argv[3])) + "/";
+	string object_path = string(argv[3],strlen(argv[3])) + "/";
+	string path_data = string(argv[4],strlen(argv[4])) + "/";
 	
 	cout << "reading db from '" << path_data << "'..." << endl;
 	read_data(path_data);
@@ -90,29 +93,38 @@ int main(int argc, char* argv[]) {
 	string input_string = "";
 	ofstream out;
 	string cpp_query = source_path + "query_1.cpp";
-	string cpp_types = source_path + "Types.cpp";
-	string cpp_schema = source_path + "schema_1.cpp";
-	string so_query = source_path + "query_1.so";
+	string o_types = object_path + "Types.cpp.o";
+	string o_schema = object_path + "schema_1.cpp.o";
+	string so_query = object_path + "query_1.so";
 	string compilation_command = "g++ "
-		+ cpp_query + " " + cpp_types + " " + cpp_schema + " "
+		+ cpp_query + " " + o_types + " " + o_schema + " "
 		+ " -std=c++11 -O3 -fPIC -shared -rdynamic -o "
 		+ so_query
 	;
+	
+	chrono::duration<int64_t, std::milli> elapsed_compilation;
+	chrono::duration<int64_t, std::milli> elapsed_execution;
+	chrono::high_resolution_clock::time_point start;
+	chrono::high_resolution_clock::time_point end;
 	while (true) {
 		try {
 			cout << "#";
 			getline(cin, input_string);
-			if (input_string == "quit") break;
+			transform(input_string.begin(), input_string.end(), input_string.begin(), ::tolower);
+			if (input_string == "quit" || input_string == "exit") break;
 			if (input_string.empty()) continue;
-			
+			//parse query
 			Parser_Query q(input_string, schema.get(), "db");
 			q.parse();
-			
+			//create temporary file
 			out.open(cpp_query, ios_base::out);
 			out << q.generate();
 			out.close();
 			//compiling
+			start = chrono::high_resolution_clock::now();
 			int res  = system(compilation_command.c_str());
+			end = chrono::high_resolution_clock::now();
+			elapsed_compilation = chrono::duration_cast<chrono::milliseconds>(end - start);
 			if (res != 0) throw runtime_error("g++ exited with error " + to_string(res));
 			//load lib
 			void* handle=dlopen(so_query.c_str(),RTLD_NOW);
@@ -121,16 +133,21 @@ int main(int argc, char* argv[]) {
 			auto fn=reinterpret_cast<void (*)(const Database&)>(dlsym(handle, "run_query"));
 			if (!fn) throw runtime_error(dlerror());
 			//run function
+			start = chrono::high_resolution_clock::now();
 			fn(db);
+			end = chrono::high_resolution_clock::now();
+			elapsed_execution = chrono::duration_cast<chrono::milliseconds>(end - start);
 			//close lib
 			if (dlclose(handle)) throw runtime_error(dlerror());
+			cout << "compilation: " << elapsed_compilation.count() << " ms" << endl
+				 << "execution:   " << elapsed_execution.count()   << " ms" << endl;
 			
 		} catch (ParserQueryError& e) {
 			cerr << e.what() << endl;
 			continue;
 		} catch (runtime_error& e) {
 			cerr << e.what() << endl;
-			exit(1);
+			continue;
 		}
 		
 	}
